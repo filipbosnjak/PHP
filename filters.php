aa6f315c369628e04fef3435c7d0aebb6d2daa20
Filip Bošnjak <filipo.bosnjak@gmail.com>
2020-10-09 14:22:44 +0200

    Filters

--- filters.php
+++ filters.php
@@ -1,3 +1,29 @@
+<?php
+/*
+if(filter_has_var(INPUT_POST,'data')){
+    echo 'Data';
+}else{
+    echo 'No Data';
+}
+*/
+//We can check if email is valid i.e. if it is typed in correct format. Using FILTER_VALIDATE_EMAIL
+
+if(filter_has_var(INPUT_POST,'data')){
+    if(!filter_input(INPUT_POST,'data',FILTER_VALIDATE_EMAIL)){
+        echo 'Email invalid';
+    }else{
+        echo 'Success';
+    }
+}
+
+//FILTER_VALIDATE_DOMAIN - integer - 277 - const -  Validates whether the domain name label lengths are valid.
+//FILTER_VALIDATE_URL Validates value as URL
+//FILTER_VALIDATE_IP Validates value as IP address
+//FILTER_VALIDATE_MAC Validates value as MAC address.
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,7 +33,10 @@
 </head>
 <body>
     <form method="POST" action="<?php echo $_SERVER['PHP_SELF']?>"> <!--Action atribute specifies where the data should be sent. Using safer POST method-->
-        <input type="text">
+        <input type="text" name="data" placeholder = "Enter your email...">
+        <input type="submit" value="Submit">
     </form>
 </body>
 </html>
+
+
